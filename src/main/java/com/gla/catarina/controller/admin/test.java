7f0a33a43fb4bda000456163be52662b892e40cf
alfred <chenchenzhonghao@163.com>
2023-07-20 21:57:56 +0100

    test

---
+++ src/main/java/com/gla/catarina/controller/admin/test.java
@@ -0,0 +1,14 @@
+package com.gla.catarina.controller.admin;
+
+/**
+ * @program: SecondHandTradingPlatform
+ * @ClassName test
+ * @description:
+ * @author: alfred-chenzhonghao
+ * @create: 2023-07-20 21:57
+ * @Version 1.0
+ **/
+public class test {
+
+    //测试git
+}
